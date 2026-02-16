2144683248993d82855d2b73094472157df44a41
Bárbara Thaís Sobral <barbarasobral87@gmail.com>
2026-02-16 19:49:57 -0300

    ajustes de skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
         <div id="skills" className="relative min-h-screen p-4 overflow-hidden">
             <img src={bgMobile} alt="Background Mobile" className="absolute inset-0 w-full h-full object-contain z-0 md:hidden" />
             <img src={bgSkills} alt="Background Skills" className="absolute inset-0 w-full h-full object-contain z-0 hidden md:block" />
-            <h1 className="relative text-oranges text-4xl font-bold mb-6 text-center z-10 py-8 md:py-3">Skills</h1>
+            <h1 className="relative text-oranges text-4xl font-bold mb-6 text-center z-10 py-12 md:py-3">Skills</h1>
         </div>
     )
 }
